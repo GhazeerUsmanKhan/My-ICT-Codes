a9f0f37be2277ecaa70eb63e9d5f67b6d97d07ba
GhazeerUsmanKhan <ghazeer.usman.khan@gmail.com>
2023-01-13 19:36:03 +0500

    commiting

---
+++ Double Array Matching.py
@@ -0,0 +1,15 @@
+a=input("Enter Your Word/Number for Group A: ")
+b=input("Enter Your Word/Number for Group A: ")
+c=input("Enter Your Word/Number for Group A: ")
+d=input("Enter Your Word/Number for Group A: ")
+e=input("Enter Your Word/Number for Group A: ")
+f=input("Enter Your Word/Number for Group B: ")
+g=input("Enter Your Word/Number for Group B: ")
+h=input("Enter Your Word/Number for Group B: ")
+i=input("Enter Your Word/Number for Group B: ")
+j=input("Enter Your Word/Number for Group B: ")
+array_A = [a,b,c,d,e]
+array_B = [f,g,h,i,j]
+print("Your options for A are ", array_A,"and your options for B are", array_B)
+if array_A == array_B:
+    print("array a is equal to array b")

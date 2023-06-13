f36f4a26b039918fb66539908f3488bda0984d33
GhazeerUsmanKhan <ghazeer.usman.khan@gmail.com>
2023-06-13 11:52:42 +0500

    Complete

--- Password Perfecting Code.py
+++ Password Perfecting Code.py
@@ -4,9 +4,9 @@ import random
 print("Welcome to the Official Piggy Bank Fiduciary Application. ")
 a=input("Since I know you are a new initiate, please enter your signup Username. ")
 while True:
-    b=input("Please enter the password for your account. Password needs to have at least 1 Uppercase, 1 Lowercase and 1 special character. Has to be a minimum of 8 letters. ")
+    password=input("Please enter the password for your account. Password needs to have at least 1 Uppercase, 1 Lowercase and 1 special character. Has to be a minimum of 8 letters. ")
     password_pattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
-    x=re.match(password_pattern, b)
+    x=re.match(password_pattern, password)
     if x:
         print("Your password is picture perfect. Please Continue.")
         break
